e1a11f247bb8c01a146e6c6bd31806bc43649f29
Sahil Garg <sahilgarg2801@gmail.com>
2024-12-25 15:58:00 +0530

    Implement decoding for bencoded Dictionary

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import com.google.gson.Gson;
 
 class Decoded {
     String stringDecoded = null;
     Long longDecoded = null;
     ArrayList<Object> listDecoded = null;
+    HashMap<String, Object> dictDecoded=null;
 
     Decoded(String stringDecoded) {
         this.stringDecoded = stringDecoded;
@@ -16,6 +19,9 @@ class Decoded {
 
     Decoded(ArrayList<Object> listDecoded) {
         this.listDecoded = listDecoded;
+    }
+    Decoded(HashMap<String, Object> dictDecoded) {
+        this.dictDecoded = dictDecoded;
     }
 }
 
@@ -45,7 +51,10 @@ public class Main {
             return new Decoded(decodeLongBencode(bencodedString));
         } else if (bencodedString.startsWith("l")) {
             return new Decoded(decodeListBencode(bencodedString));
-        } else if (Character.isDigit(bencodedString.charAt(0))) {
+        }else if(bencodedString.startsWith("d")) {
+        	return new Decoded(decodeDictBencode(bencodedString));
+        }
+        else if (Character.isDigit(bencodedString.charAt(0))) {
             return new Decoded(decodeStringBencode(bencodedString));
         } else {
             throw new RuntimeException("Invalid bencoded format");
@@ -61,6 +70,77 @@ public class Main {
     static Long decodeLongBencode(String bencodedString) {
         String value = bencodedString.substring(1, bencodedString.indexOf('e'));
         return Long.parseLong(value);
+    }
+    
+    static HashMap<String,Object> decodeDictBencode(String bencodedString){
+    	HashMap<String,Object> dictionary=new HashMap<String, Object>();
+//    	System.out.println(dictionary);
+    	int index=1;
+//    	System.out.println(index);
+    	int key=1;
+		String DictKey=null;
+		Object DictValue=null;
+    	while(index<bencodedString.length()-1) {
+    		char current=bencodedString.charAt(index);
+//    		System.out.println(index+"------------------"+current);
+    		//let key=1;
+    		//if key=1 then means we have key other wise we have value;\
+    		
+    		
+    		if(Character.isDigit(current)) {
+    			String strValue=decodeStringBencode(bencodedString.substring(index));
+//    			System.out.println(strValue+"---------------------------------------------");
+    			if(key==1) {
+    				DictKey=strValue;
+    				key=0;
+    			}else {
+    				DictValue=strValue;
+    				key=1;
+    			}
+    			index += strValue.length() + String.valueOf(strValue.length()).length() + 1;
+//    			System.out.println(index);
+    			
+    		}
+    		else if(current == 'i') {
+    			String intPart = bencodedString.substring(index);
+                Long intValue = decodeLongBencode(intPart);
+//                System.out.println(intPart+"-----------------"+intValue);
+                DictValue=intValue;
+//                System.out.println("DictValue: "+DictValue);
+                index += String.valueOf(intValue).length() + 2;
+                key=1;
+    		}
+    		else if(current=='l') {
+    			String sublistStr=findNextBencodeBlock(bencodedString.substring(index));
+    			ArrayList<Object> list=decodeListBencode(bencodedString);
+    			DictValue=list;
+    			index += sublistStr.length();
+    			key=1;
+    			
+    		}
+    		else if(current=='d') {
+//    			String sublist = findNextBencodeBlock(bencodedString.substring(index));
+//                list.add(decodeListBencode(sublist));
+//                index += sublist.length();
+    			
+    			String subDict=findNextBencodeBlock(bencodedString.substring(index));
+    			DictValue=decodeDictBencode(subDict);
+    			index=index+subDict.length();
+    			key=1;
+    			
+    		}else {
+                throw new RuntimeException("Invalid bencoded list format");
+            }
+    		if(key==1) {
+    			dictionary.put(DictKey,DictValue);
+    		}
+    	}
+    	
+    	
+    	
+    	
+		return dictionary;
+    	
     }
 
     static ArrayList<Object> decodeListBencode(String bencodedString) {
@@ -81,7 +161,12 @@ public class Main {
                 String sublist = findNextBencodeBlock(bencodedString.substring(index));
                 list.add(decodeListBencode(sublist));
                 index += sublist.length();
-            } else {
+            }else if (current == 'd') {
+            	String subDict=findNextBencodeBlock(bencodedString.substring(index));
+                list.add(decodeDictBencode(subDict));
+                index += subDict.length();
+            }
+            else {
                 throw new RuntimeException("Invalid bencoded list format");
             }
         }
@@ -91,24 +176,17 @@ public class Main {
     static String findNextBencodeBlock(String bencodedString) {
         int balance = 0;
         int endIndex = 0;
-//        System.out.println(bencodedString.length());
         for (int i = 0; i < bencodedString.length(); i++) {
             char current = bencodedString.charAt(i);
-//            System.out.println(i+" -----------------------------------"+current);
-            if (current == 'l'||current=='i') balance++;
+            if (current == 'l'||current=='i'||current=='d') balance++;
             if (current == 'e') balance--;
             if(current==':') {
             	
-//            	int len=Character.getNumericValue(bencodedString.charAt(i-1));
-//            	Character.getNumericValue(char)
             	int startIndex=i-1;
             	while(Character.isDigit(bencodedString.charAt(startIndex)) ) {
-            		
-//            		System.out.println(startIndex+"--------hahahaha-----------"+Character.isDigit(bencodedString.charAt(startIndex)));
             		startIndex--;
             	}
             	int len=Integer.valueOf(bencodedString.substring(startIndex+1,i)); 
-//            	System.out.println();
             	i=i+len;
             }
             if (balance == 0) {
